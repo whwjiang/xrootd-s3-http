7f31273632e44afca89379c1cd6b0f52fc9cbd37
William Jiang <w.jiang156@gmail.com>
2025-08-08 18:49:51 +0000

    fixup! allow multiple expected response code

--- src/HTTPCommands.cc
+++ src/HTTPCommands.cc
@@ -71,7 +71,7 @@ size_t HTTPRequest::handleResults(const void *ptr, size_t size, size_t nmemb,
 
 	// std::cout << "Handling results with size " << (size * nmemb) << " and
 	// HTTP verb " << me->httpVerb << "\n";
-    if (me->httpVerb == "GET") {
+	if (me->httpVerb == "GET") {
 		if (!me->responseCode) {
 			auto rv = curl_easy_getinfo(
 				me->m_curl_handle, CURLINFO_RESPONSE_CODE, &(me->responseCode));
@@ -81,7 +81,7 @@ size_t HTTPRequest::handleResults(const void *ptr, size_t size, size_t nmemb,
 				return 0;
 			}
 		}
-        if (me->expectedResponseCode.count(me->getResponseCode()) &&
+		if (me->expectedResponseCode.count(me->getResponseCode()) &&
 			me->requestResult() != nullptr) {
 			if (!me->m_result_buffer_initialized) {
 				me->m_result_buffer_initialized = true;
@@ -799,26 +799,25 @@ void HTTPRequest::ProcessCurlResult(CURL *curl, CURLcode rv) {
 		return;
 	}
 
-    if (!expectedResponseCode.count(responseCode)) {
-        errorCode = "E_HTTP_RESPONSE_NOT_EXPECTED";
-        errorMessage = m_result;
-        if (errorMessage.empty()) {
-            std::ostringstream ss;
-            ss << "HTTP response was " << responseCode
-               << ", expected one of {";
-            bool first = true;
-            for (const auto &code : expectedResponseCode) {
-                if (!first) {
-                    ss << ", ";
-                }
-                first = false;
-                ss << code;
-            }
-            ss << "}, and no body was returned.";
-            errorMessage = ss.str();
-        }
-        return;
-    }
+	if (!expectedResponseCode.count(responseCode)) {
+		errorCode = "E_HTTP_RESPONSE_NOT_EXPECTED";
+		errorMessage = m_result;
+		if (errorMessage.empty()) {
+			std::ostringstream ss;
+			ss << "HTTP response was " << responseCode << ", expected one of {";
+			bool first = true;
+			for (const auto &code : expectedResponseCode) {
+				if (!first) {
+					ss << ", ";
+				}
+				first = false;
+				ss << code;
+			}
+			ss << "}, and no body was returned.";
+			errorMessage = ss.str();
+		}
+		return;
+	}
 
 	return;
 }
@@ -829,14 +828,14 @@ HTTPUpload::~HTTPUpload() {}
 
 bool HTTPUpload::SendRequest(const std::string &payload) {
 	httpVerb = "PUT";
-    expectedResponseCode = {200, 201};
+	expectedResponseCode = {200, 201};
 	return SendHTTPRequest(payload);
 }
 
 bool HTTPUpload::StartStreamingRequest(const std::string_view payload,
 									   off_t object_size) {
 	httpVerb = "PUT";
-    expectedResponseCode = {200, 201};
+	expectedResponseCode = {200, 201};
 	headers["Content-Type"] = "binary/octet-stream";
 	return sendPreparedRequest(hostUrl, payload, object_size, false);
 }
@@ -878,7 +877,7 @@ bool HTTPDownload::SendRequest(off_t offset, size_t size) {
 		formatstr(range, "bytes=%lld-%lld", static_cast<long long int>(offset),
 				  static_cast<long long int>(offset + size - 1));
 		headers["Range"] = range.c_str();
-        this->expectedResponseCode = {206};
+		this->expectedResponseCode = {206};
 	}
 	m_log.Log(LogMask::Debug, "HTTPDownload::SendRequest",
 			  "Sending GET request");
@@ -904,7 +903,7 @@ HTTPDelete::~HTTPDelete() {}
 
 bool HTTPDelete::SendRequest() {
 	httpVerb = "DELETE";
-    this->expectedResponseCode = {204};
+	this->expectedResponseCode = {204};
 	includeResponseHeader = true;
 	std::string noPayloadAllowed;
 	return SendHTTPRequest(noPayloadAllowed);
